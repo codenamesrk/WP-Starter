ac66995e278dfb6a37d4fd97a5cb65e0f6fd011a
codenamesrk <srijit@outlook.com>
2017-04-06 01:46:04 +0530

    changed the way webfont loader loads fonts

--- assets/javascript/custom/font-loader.js
+++ assets/javascript/custom/font-loader.js
@@ -1,9 +1,15 @@
 // Web Font Loader
-(function(){
-  WebFont.load({
-    google: {
-      families: ['Playfair+Display:400,700', 'Playfair+Display:400,700']
-    }
-  });	
+window.WebFontConfig = {
+	google: {
+	  families: ['Playfair+Display:400,700', 'Playfair+Display:400,700']
+	}
+};
+(function() {
+	var wf = document.createElement('script');
+	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+	wf.type = 'text/javascript';
+	wf.async = 'true';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(wf, s);
 })();
 
