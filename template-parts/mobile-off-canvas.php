b70d389cb454a96e72a8b89a91a3e473511d8fe9
codenamesrk <srijit@outlook.com>
2017-04-11 18:44:06 +0530

    redesigned topbar

--- template-parts/mobile-off-canvas.php
+++ template-parts/mobile-off-canvas.php
@@ -8,7 +8,27 @@
 
 ?>
 
-<nav class="off-canvas position-left" id="offCanvas" data-off-canvas role="navigation">
+<nav class="off-canvas position-left" id="offCanvas" data-off-canvas role="navigation" data-transition="overlap">
   <?php foundationpress_mobile_nav(); ?>
+  <button class="close-button" aria-label="Close menu" type="button" data-close>
+  	<span aria-hidden="true">&times;</span>
+  </button>	
 </nav>
+
+<nav class="off-canvas site-search position-top" id="siteSearch" data-off-canvas role="search" data-transition="overlap">
+	<div class="site-search__bar">
+		<div class="site-search__input">
+			<form action="" class="searchform">
+				<input type="text" name="s" value="" placeholder="Start Typing Here">
+				<input type="submit" id="searchsubmit" value="Search" class="hide">
+			</form>			
+		</div>
+		<div class="site-search__close">
+			<button class="close-button" aria-label="Close menu" type="button" data-close>
+				<span aria-hidden="true">&times;</span>
+			</button>			
+		</div>
+	</div>	
+</nav>
+
 <div class="off-canvas-content" data-off-canvas-content>
