e97714d274d7b4f92bf7b93b9122edf9a47c0c05
codenamesrk <srijit@outlook.com>
2017-04-28 11:48:49 +0530

    stylize blog lists page

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -9,7 +9,10 @@
 	<?php if(is_page('about')) : ?>
 	<h2 class="sec-about"><?php wp_title(''); ?></h2>
 	<?php elseif(is_page('publications')) : ?>
-	<h2 class="sec-pub"><?php wp_title(''); ?></h2>	
+	<h2 class="sec-pub"><?php wp_title(''); ?></h2>
+	<?php elseif(is_home()) : ?>
+	<h2 class="sec-muse"><?php wp_title(''); ?></h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi soluta dolorem ipsam voluptate cupiditate porro similique quas vero eos quo ducimus neque accusamus perspiciatis blanditiis, aperiam possimus aliquid quod inventore.</p>		
 	<?php else : ?>
 	<h2 class="sec-muse"><?php wp_title(''); ?></h2>		
 	<?php endif; ?>
