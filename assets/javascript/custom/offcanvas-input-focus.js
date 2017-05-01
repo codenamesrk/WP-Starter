b65772a1089846d61e45c8413056fdfde826002c
codenamesrk <srijit@outlook.com>
2017-05-02 02:39:21 +0530

    contact page and footer logo changes

--- assets/javascript/custom/offcanvas-input-focus.js
+++
@@ -1,7 +0,0 @@
-$("#siteSearch").on("opened.zf.offcanvas", function () {
-	$('#search-term').focus();
-	if( !$('#search-term').is(':focus') ) {
-		$('#search-term').focus();
-	}	
-	console.log('hit');
-});
